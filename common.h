0a54fa88c70f00ed5fe380e4fea92d4022b50c31
fulgencio <fulgencio.lopez.serrano@sparkbit.pl>
2023-03-29 20:01:12 +0200

    feat: add basic arena and dynamic arrays

--- common.h
+++ common.h
@@ -13,6 +13,14 @@
 #include <assert.h>
 
 
+typedef uint8_t U8;
+typedef int8_t I8;
+typedef uint32_t U32;
+typedef int32_t I32;
+typedef uint64_t U64;
+typedef int64_t I64;
+
+
 typedef struct Vec2 {
     float x;
     float y;
